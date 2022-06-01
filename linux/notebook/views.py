cade322d16f186b41742d5b2fffd0f1229c28f91
hanieith <shkutkoff@gmail.com>
2022-06-01 16:43:30 +0300

    01.06.2022

--- linux/notebook/views.py
+++ linux/notebook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    name = 'hanieith'
+    context = {
+        'name':name
+    }
+    return render(request, template_name='notebook/index.html', context=context)
