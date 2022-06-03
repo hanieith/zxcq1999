983f5e17ef1fb8a5f2448735d799b71789754291
hanieith <shkutkoff@gmail.com>
2022-06-03 12:37:49 +0300

    03.06.2022

--- linux/notebook/models.py
+++ linux/notebook/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 
 class Names(models.Model):
-    number = models.IntegerField()
     name = models.JSONField()
-
-    def __str__(self):
-        return self.number
--- linux/notebook/views.py
+++ linux/notebook/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from .models import Names
+import json
+from django.http import HttpResponseRedirect
 
 def index(request):
-    name = 'hanieith'
-    context = {
-        'name':name
-    }
-    return render(request, template_name='notebook/index.html', context=context)
+    if request.method == 'POST':
+        json_dict = request.POST.dict()
+        json_dict.pop('csrfmiddlewaretoken')
+        json_resp = json.dumps(json_dict)
+        json_save = Names.objects.create(name=json_resp)
+        json_save.save()
+        return HttpResponseRedirect('/view/')
+    else:
+        return render(request, template_name='notebook/index.html')
+
 
 def view_names(request):
-    return render(request, template_name='notebook/view_names.html')
+    submits = Names.objects.all()
+    context = {'names':submits}
+    return render(request, template_name='notebook/view_names.html', context=context)
